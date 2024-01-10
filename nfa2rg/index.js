d78d4a0db8425fa8a04dc10012bf4e05f5ed673c
Sim Cheng Seng <josephsim0501@gmail.com>
2024-01-10 11:42:22 +0800

    UI and some js for NFAtoRG

--- nfa2rg/index.js
+++ nfa2rg/index.js
@@ -1,1 +1,34 @@
 document.getElementById("random").innerHTML = "NFA -> RG"
+
+function getInput(id) {
+    var value = document.getElementById(id).value;
+
+    var inputText = [];
+    var index = 0;
+    var newIndex = true;
+    for (var i = 0; i < value.length; i++) {
+        if (newIndex) {
+            inputText[index] = value[i];
+            newIndex = false;
+        } else if (value[i] != '\n') {
+            inputText[index] += value[i];
+        } else {
+            index++;
+            newIndex = true;
+        }
+    }
+    console.log(inputText)
+    return inputText;
+}
+
+function insertEpsilon() {
+    var text = document.getElementById('string-input');
+    text.value += '\u03B5';
+}
+
+// ------------------------ convertRGtoNFA module ----------------------------------------
+
+function convertNFAtoRG() {
+    
+}
+
